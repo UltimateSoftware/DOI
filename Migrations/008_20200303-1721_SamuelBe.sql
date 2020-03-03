690de1d0609c62badcd1437645fd0d9c40915207
sbendayan <sam_bendayan@ultimatesoftware.com>
2020-03-03 17:36:07 -0500

    Added logic for dependency tree by view in single function

---
+++ Migrations/008_20200303-1721_SamuelBe.sql
@@ -0,0 +1,13 @@
+﻿-- <Migration ID="7caa18b5-2f16-4aa2-b72c-869a09b5542b" />
+GO
+
+PRINT N'Dropping [DDI].[fnRefreshMetadataForView]'
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[DDI].[fnRefreshMetadataForView]') AND (type = 'IF' OR type = 'FN' OR type = 'TF'))
+DROP FUNCTION [DDI].[fnRefreshMetadataForView]
+GO
+PRINT N'Dropping [DDI].[fnGetRefreshMetadataSPsForView]'
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[DDI].[fnGetRefreshMetadataSPsForView]') AND (type = 'IF' OR type = 'FN' OR type = 'TF'))
+DROP FUNCTION [DDI].[fnGetRefreshMetadataSPsForView]
+GO
